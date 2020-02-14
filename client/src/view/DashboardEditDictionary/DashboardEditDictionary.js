48041407b66b66c8e8854677efde3ef00ed8bc2c
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-02-14 11:14:50 +0100

    edit dictionary show currect words

--- client/src/view/DashboardEditDictionary/DashboardEditDictionary.js
+++ client/src/view/DashboardEditDictionary/DashboardEditDictionary.js
@@ -7,16 +7,41 @@ import './dashboardEditDictionary.sass';
 class DashboardEditDictionary extends React.Component {
 
     state = {
-
+        dictionary: null
     }
 
+    componentDidMount() {
+        const { config } = this.props;
+        const id = this.props.match.params.id;
+
+        try {
+            fetch(`${config.api}/api/dictionary/getone/${id}`, { method: 'POST' })
+                .then(r => r.json())
+                .then(r => {
+                    this.setState({ dictionary: r.dictionary })
+                })
+        }
+        catch {
+
+        }
+    }
 
     render() {
+        const { dictionary } = this.state;
+        let words = null;
+        if (dictionary !== null) {
+            const vocabulary = dictionary.vocabulary;
+            words = vocabulary.map((word, index) => <EditDictionaryField key={index} pl={word.pl} en={word.en} />)
+        }
+
         return (
             <div className="dashboard-edit-dictionary">
-                <div className="dashboard-edit-dictionary__container">
-                    <EditDictionaryField pl="pl" en="en" />
-                </div>
+                {
+                    dictionary !== null &&
+                    <div className="dashboard-edit-dictionary__container">
+                        {words}
+                    </div>
+                }
             </div>
         );
     }
