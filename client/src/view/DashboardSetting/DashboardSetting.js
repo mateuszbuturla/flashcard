7444d0dd14e0967b6487728ed859558048e22103
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-03-01 14:56:13 +0100

    dashboard main setting dictionary create edit style ordering

--- client/src/view/DashboardSetting/DashboardSetting.js
+++ client/src/view/DashboardSetting/DashboardSetting.js
@@ -12,12 +12,12 @@ class DashboardSetting extends React.Component {
         const { user, config } = this.props;
 
         return (
-            <div className="dashboard-setting">
-                <h2 className="dashboard-setting__header">Witaj {user.login}</h2>
-                <Link to='/dashboard/setting/changeusername' className="dashboard-setting__link">
+            <div className="dashboardSetting">
+                <h2 className="dashboardSetting__header">Witaj {user.login}</h2>
+                <Link to='/dashboard/setting/changeusername' className="dashboardSetting__link">
                     <p>Zmień nazwę użytkownika</p>
                 </Link>
-                <Link to='/dashboard/setting/changepassword' className="dashboard-setting__link">
+                <Link to='/dashboard/setting/changepassword' className="dashboardSetting__link">
                     <p>Zmień hasło</p>
                 </Link>
                 <Switch>
