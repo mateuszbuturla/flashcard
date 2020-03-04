6823fbcb3448a4a210dc17782be3bf921981fc1a
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-03-04 15:21:40 +0100

    fix save test result api

--- server/controllers/testResultController.js
+++ server/controllers/testResultController.js
@@ -5,7 +5,7 @@ exports.saveTestResult = async (req, res) => {
     const { dictionaryid, correct, incorrect } = req.params;
     try {
         if (dictionaryid && correct && incorrect) {
-            testResultModel.create({ _id: mongoose.Types.ObjectId(), dictionaryId: dictionaryid, correct: correct, incorrect: incorrect }, (err) => {
+            testResultModel.create({ _id: mongoose.Types.ObjectId(), dictionaryId: dictionaryid, correct: JSON.parse(correct), incorrect: JSON.parse(incorrect) }, (err) => {
                 if (err)
                     return console.log(err)
 
